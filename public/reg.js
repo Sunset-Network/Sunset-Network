cabd934b75883c2f73616a89f0e0dd9a6a0786ae
woofplayz <95726341+woofplayz@users.noreply.github.com>
2024-05-13 22:41:35 +0000

    fixed ads fr this time

--- public/reg.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = '//pl23239976.highcpmgate.com/c8/c6/a0/c8c6a061927d80bdcff74cd82312ee64.js';
-document.head.appendChild(script);
